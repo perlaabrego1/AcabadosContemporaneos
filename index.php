58d7cc2b1b7738d8e83514494d38853a0064c708
Yudith Rubio <yudiht17@gmail.com>
2020-12-10 00:49:26 -0800

    diseño

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
         <script src="scripts/jquery-3.4.1.js"></script>
         <script src="scripts/bootstrap.js"></script>
         <style type="text/css">
-            body{background-image:url(img/fondo.jpg);  background-size: cover;color:black; text-align: center;}   
+            body{background-image:url(img/fondos.jpg);  background-size: cover;color:black; text-align: center; }   
             * {padding: 0; margin: 0; font-family: century gothic; text-align: center;}
-            form {box-shadow: inset 0 0 0 var(--border-size) currentcolor;padding: 50px 50px; background-color: #001a1a66;  margin-top: 70px; padding-top: 28px; margin-bottom: 50px}
+            form {box-shadow: inset 0 0 0 var(--border-size) currentcolor;padding: 50px 50px;   margin-top: 70px; padding-top: 28px; margin-bottom: 50px}
             strong{text-align: center; padding: 12px; color: #F7F9F9; font-family: century gothic; font-size:30px }
             h4{text-align: center; padding: 12px; color: #F7F9F9; font-family: century gothic; font-size:25px }
             button {width: 200px; padding: 9px; margin: auto; margin-top: 12px; font-size: 20px}
             button[type='submit']{background-color: #000066; color: #fff; width: 300px; margin: 0 10%; margin-top: 50px; border: none;}
             .button:hover {background-color: #006699}     
-            #texto{font-family: 'Lucida Sans', 'Lucida Sans Regular',  'Lucida Sans Unicode', Geneva, Verdana, sans-serif;text-align: center;font-size:50px; padding: 12px;color:black; }   
+            #texto{background-color:#001a1a66;color: #e0dfdc;letter-spacing: .1em;text-shadow: 0 -1px 0 #fff, 0 1px 0 #2e2e2e, 0 2px 0 #2c2c2c, 0 3px 0 #2a2a2a, 0 4px 0 #282828, 0 5px 0 #262626, 0 6px 0 #242424,  0 22px 30px rgba(0, 0, 0, 0.9);}   
+            #texto2{color: #1f262e; letter-spacing: .1em;text-shadow: -1px -1px 1px #111 #363636; font-size: 60px;}   
+        
         </style>
     </head>
     
@@ -24,15 +26,16 @@
                 <img id="usuario" src="img/logo.png" ></img>
             </div>
         </nav>
+        <div id="texto2">
+        <strong id="texto2">¡BIENVENIDO!</strong>
+            </div>
         <div id="texto">
-        <strong id="texto">¡BIENVENIDO!</strong>
-        </div>
         <form id = "form-ingresar" action="registro.php" method="post">
             <strong>SOMOS DISTRIBUIDORES</strong>
             <h4>DIRECTOS DE LAS MEJORES MARCAS</h4>
             <button  type="submit" class="button">Ingresar</button>
         </form>
-         
+        </div>
     </body>
     
 </html>
--- registro.php
+++ registro.php
@@ -6,23 +6,27 @@
         <script src="scripts/jquery-3.4.1.js"></script>
         <script src="scripts/bootstrap.js"></script>
         <style type="text/css">
-            body{background-image:url(img/fondo.jpg);  background-size: cover;color:black;}   
+            body{background-image:url(img/login3.jpg);  background-size: cover;color:black;}   
             * {padding: 0; margin: 0; font-family: century gothic; text-align: center;}
-            form {border-radius: 20px;box-shadow: inset 0 0 0 var(--border-size) currentcolor;padding: 50px 50px; background-color: #001a1a66; margin: calc(25% + 50px); margin-top: 70px; padding-top: 28px; margin-bottom: 50px}
+            form {border-radius: 20px;box-shadow: inset 0 0 0 var(--border-size) currentcolor;padding: 40px 40px; background-color: #001a1a66; margin: calc(25% + 60px); margin-top: 40px; padding-top: 15px; margin-bottom: 0px}
             strong{text-align: center; padding: 12px; color: #F7F9F9; font-family: century gothic; font-size:30px }
             h4{text-align: center; padding: 12px; color: #F7F9F9; font-family: century gothic; font-size:25px }
             button {width: calc(100% - 20px); padding: 9px; margin: auto; margin-top: 12px; font-size: 20px}
             button[type='submit']{background-color: #000066; color: #fff;  width: 300px; margin: 0 10%; margin-top: 50px; border: none;}
             .button:hover {background-color: #006699} 
             .boton1{background-color: #F7F9F9; width: 200px; padding: 9px; margin: auto;  font-size: 20px; color: black;  border: none;border-width: 1px;border-style: solid;border-color: black;}
-            input{font-size:20px}       
+            input{font-size:20px}
+            #empleado{font-size:20px}
+            #usuario{width: 30%; height: 25%;}
+            #datos{font-size:30px; color: black;background-color: #66ff33;     transition: all .5s ease;}
+            #datos2{text-align: center;}       
         </style>
     </head>
     
     <body>
         <nav class="navbar navbar-light" style="background-color: #F7F9F9">
             <div class="navbar-brand">
-                <img id="usuario" src="img/logo.png" ></img>
+                <img src="img/logo.png" ></img>
             </div>
             <div>
                 <button onclick="location.href='index.php'" class="boton1">Salir</button>
@@ -41,23 +45,26 @@
                     $cant_filas = mysqli_num_rows($consulta);
                     if($cant_filas != 0)
                     {
-                        echo "DATOS CORRECTOS";
+                        echo "<h2 id='datos2'><span id=datos>DATOS CORRECTOS</span></h2>";
                         #header("Location: registro.php");
                         #header("Location: usr_admin.php?varUsr=$usr"); Se usaría para llevarlo al perfil de administrador
                     }
                     else
-                        echo "DATOS INCORRECTOS";
+                        echo "<h2 id='datos2'><span id=datos>DATOS INCORRECTOS</span></h2>";
                 }
                 mysqli_close($con_mysql);
             }
         ?>
         <form id = "form-ingresar" action="registro.php" method="post">
             <strong>INGRESE SU ID Y CONTRASEÑA</strong>
+            <div class="form-group" align="center">
+                    <img id="usuario" src="img/usuario.png" ></img>
+                </div>
             <h4></h4>
             <!--<input type=text name= "inp_usr"  placeholder = "ID" required> </input>-->
             <?php
                 $con_mysql = mysqli_connect("127.0.0.1", "root", "", "checador_db") or die ("Problemas de conexión");
-                echo'<select name = inp_usr required>';
+                echo'<select id=empleado name = inp_usr required>';
                 $comando = "call consultaListaEmp();";
                 $consulta = mysqli_query($con_mysql, $comando);
                 $cant_filas = mysqli_num_rows($consulta);
